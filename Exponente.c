f632d5ea0e497ae4eb6505afba610851aad5be25
SET-DET <69767069+linguini007@users.noreply.github.com>
2023-06-04 11:12:44 -0600

    Add files via upload

---
+++ Exponente.c
@@ -0,0 +1,29 @@
+//Escribir un programa en C que implemente una función para calcular la potencia de un número.
+// La función debe recibir como parámetros la base y el exponente y regresar la potencia calculada.
+#include "stdio.h"
+int expo(int base, int potencia);
+int main (void){
+	int base,potencia;
+	printf("Dame la base: ");
+	scanf("%d",&base);
+	printf("Dame la Potencia: ");
+	scanf("%d",&potencia);
+	
+	
+	printf("%d",expo(base,potencia));
+	return 0;
+} 
+ 
+ int expo(int base, int potencia){
+ 	int i,x,res=1;
+ 		for(i=0; i<potencia ;i++){
+ 			res = res *base;
+		 }
+ 	return (res);
+ }
+ 
+ 
+ 
+ 
+
+
